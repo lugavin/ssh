99582f160f077822f15e5f2712b02312b45a0dd2
lugavin <lugavin@outlook.com>
2017-06-18 09:49:44 +0800

    Update

--- ssh-common-web/src/main/java/com/ssh/common/web/controller/AbstractController.java
+++ ssh-common-web/src/main/java/com/ssh/common/web/controller/AbstractController.java
@@ -7,4 +7,9 @@ public abstract class AbstractController {
     //     binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat(Constant.DATETIME_FORMAT), Boolean.TRUE));
     // }
 
+    // @RequestMapping(value = "/{root}/{page}", method = {RequestMethod.GET})
+    // protected String toPage(@PathVariable String root, @PathVariable String page) throws Exception {
+    //     return root + "/" + page;
+    // }
+
 }
